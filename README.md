206b42d02eff2b1bad53959b3a1ccfa1e915cb4d
ccxz84 <ccxz84@naver.com>
2022-03-21 18:23:16 +0900

    initail commit

--- README.md
+++
@@ -1,2 +0,0 @@
-# pet-api
-api server
---
+++ src/test/kotlin/com/animal/house/was/WasApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.animal.house.was
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class WasApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
