126ee205de089f86b4db14fa51e834828a2b9db5
ccxz84 <ccxz84@naver.com>
2022-04-10 21:55:24 +0900

    [chore] 불필요한 import 제거

--- src/main/kotlin/com/animal/house/was/WasApplication.kt
+++ src/main/kotlin/com/animal/house/was/WasApplication.kt
@@ -2,7 +2,6 @@ package com.animal.house.was
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
-import org.springframework.web.servlet.config.annotation.EnableWebMvc
 
 @SpringBootApplication
 class WasApplication
